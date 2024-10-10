d69f92fb1aaab202e6143a6d72239fb93269af46
Shiekh Hammad <146128383+MUHAMMADHAMMADKALAM@users.noreply.github.com>
2024-10-11 00:06:07 +0500

    first commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div id='hero' className='min-h-screen bg-no-repeat bg-[url(/hammad.jpg)] bg-left lg:bg-[15%] bg-cover'
+    style={
+      {backgroundSize: "35"}
+    }
+    >
+      
+    </div>
+  )
+}
+
+export default Hero
